091f9ae9d4d1e3bea56999dbc0f2f4fec92afc74
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2023-02-23 00:20:26 +0300

    action updated

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,16 @@
+import React from "react";
 import type { LinksFunction, ActionArgs } from "@remix-run/node";
-import { useActionData } from "@remix-run/react";
+import { useActionData, useNavigation, useTransition } from "@remix-run/react";
 
 import Form from "../../components/form";
 
 import style from "../styles/index.css";
 import formStylesUrl from "../styles/form.css";
+
+const API_KEY = process.env.API_KEY;
+const API_SECRET = process.env.API_SECRET;
+const FORM_ID = process.env.FORM_ID;
+const API = "https://api.convertkit.com/v3";
 
 export const links: LinksFunction = () => {
   return [
@@ -17,16 +23,31 @@ export const action = async ({ request }: ActionArgs) => {
   await new Promise((res) => setTimeout(res, 1000));
   const Iform = await request.formData();
   const email = Iform.get("email");
-  console.log(email);
 
-  return { ok: true };
+  const res = await fetch(`${API}/forms/${FORM_ID}/subscribe`, {
+    method: "post",
+    body: JSON.stringify({ email, api_key: API_KEY }),
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  });
+
+  return res.json();
 };
 
 export default function Index() {
   const data = useActionData<typeof action>();
-  console.log(data);
+  const navigation = useNavigation();
+  let state: any = navigation.formData;
+
+  React.useEffect(() => {
+    console.log(navigation);
+  }, [state]);
+
+  console.log(navigation.formAction);
   return (
     <div className="container">
+      {state}
       <Form />
     </div>
   );
--- components/form.tsx
+++ components/form.tsx
@@ -2,15 +2,9 @@ import React from "react";
 import type { MetaFunction } from "@remix-run/node";
 import { Form } from "@remix-run/react";
 
-const API_KEY = process.env.API_KEY;
-const API_SECRET = process.env.API_SECRET;
-const FORM_ID = process.env.FORM_ID;
-const API = "https://api.convertkit.com/v3";
-
 function IForm() {
-  console.log(API_KEY);
   return (
-    <Form method="post">
+    <Form replace method="post">
       <h2>Subscribe!</h2>
       <p>Don't miss any of the action!</p>
       <fieldset>
